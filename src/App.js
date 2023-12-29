d2c5859a2b93ca4381c022994a271ab42f08734a
Almaz Balbaev <kujitengu@gmail.com>
2023-12-29 19:45:03 +0600

    added fetching logic added some html

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import axiosApi from "../axios-api";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="header">
-        Header
-      </div>
-      <div className="main-block">
+    const [state, setState] = useState([]);
 
-      </div>
-    </div>
-  );
+    const fetchData = async () => {
+      try {
+          const response = await axiosApi.get('/');
+          const newState = Object.entries(response.data).map(([key, value]) => ({
+              id: key,
+              ...value,
+          }));
+          setState(newState);
+          console.log(newState);
+      } catch (e) {
+          console.error('Error fetching data: ', e);
+      }
+    };
+
+    useEffect(() => {
+        fetchData().then();
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="header">
+                Annual planer app
+            </div>
+            <div className="action-panel">
+                <button>Add new month</button>
+                <button>Add new plan</button>
+            </div>
+            <div className="main-block">
+                {state.map((item) => (
+                    <div className="month" key={item.id}>{item.id}</div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
