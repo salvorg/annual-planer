6cec67174ce4a956da8959364f772a29cae26f2a
Almaz Balbaev <kujitengu@gmail.com>
2024-01-01 23:50:29 +0600

    - changed logic

--- src/components/UI/select/MySelect.js
+++ src/components/UI/select/MySelect.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import styles from './MySelect.module.css';
 
 const MySelect = ({options, defaultValue, value, onChange, name}) => {
     return (
         <select
+            className={styles.mySelect}
             value={value}
             onChange={event => onChange(event)}
             name={name}
         >
             <option disabled value="">{defaultValue}</option>
             {options.map(option =>
-                <option key={option.name} value={option.name}>
-                    {option.name}
+                <option key={option.number ? option.number : option} value={option.name ? option.name : option}>
+                    {option.name ? option.name : option}
                 </option>
             )}
         </select>
