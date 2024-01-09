4b0f942da844618597878b471b6846880d92d8c7
Almaz Balbaev <kujitengu@gmail.com>
2024-01-09 15:14:10 +0600

    - added router logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import Home from "./containers/Home";
+import {Link , Route, Routes} from "react-router-dom";
+import Month from "./containers/Month";
 
 function App() {
     return (
         <div className="App">
             <div className="header">
-                Annual planer app
+                <Link className="navLink" to="/">Annual planer app</Link>
             </div>
-            <Home/>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Home/>
+                    }
+                />
+                <Route
+                    path="/plans/:id"
+                    element={
+                        <Month/>
+                    }
+                />
+            </Routes>
         </div>
     );
 }
