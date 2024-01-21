a5d12eb9aae466c9f27f6d3db98861771a7b36a9
Almaz Balbaev <kujitengu@gmail.com>
2024-01-21 21:45:00 +0600

    - added input handler and state

--- src/components/PlanModalForm.js
+++ src/components/PlanModalForm.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './PlanModalForm.module.css';
 
 const PlanModalForm = ({ isOpen, onClose }) => {
+    const [state, setState] = useState({
+        title: '',
+
+    });
+
+    const inputHandler = (event) => {
+        const {name, value} = event.target;
+
+        setState((prevState) => ({ ...prevState, [name]: value }));
+    };
+
     return (
         <div className={`${styles.modal} ${isOpen ? `${styles.open}` : ''}`}>
             <div className={styles.modalContent}>
                 <span className={styles.close} onClick={onClose}>&times;</span>
-                <p>modalContent</p>
+
+                <input onChange={inputHandler} placeholder="month number" name="number" value={state.number}/>
+                <input onChange={inputHandler} placeholder="year" name="year" value={state.year}/>
             </div>
         </div>
     );
